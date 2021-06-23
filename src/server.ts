37c0f4ed07a8cd922de8043db1a7fb42f01325c0
Adryane Fernandes <edryfernandes@gmail.com>
2021-06-22 23:41:40 -0300

    tratamento de erro pelo middleare no server e lib express-async-error

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,24 @@
-import "reflect-metadata"
-import express from 'express'
+import "reflect-metadata";
+import express, { NextFunction, Request, Response } from "express";
+import "express-async-errors"
 
-import "./database"
+import "./database";
 
-import { router } from "./routes"
+import { router } from "./routes";
 
-const app = express()
-app.use(express.json())
+const app = express();
+app.use(express.json());
 
-app.use(router)
+app.use(router);
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  if (err instanceof Error) {
+    return res.status(400).send({ message: err.message });
+  }
+
+  return res.status(500).send({ message: "Internal Server Error" });
+});
 
 app.listen(3003, () => {
-  console.log("Servis is running in http://localhost:3003")
-})
+  console.log("Servis is running in http://localhost:3003");
+});
